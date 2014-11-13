82fc91f8cea0bd97b8a8b557ae738d9fb0b42481
Thibault Milan <thibault.milan@vesperiagroup.com>
2014-11-13 15:47:08 +0100

    Some reformat ... hope it will be the last time thx to editorconfig

--- views/js/pciCreator/dev/graphLineAndPointInteraction/runtime/graphLineAndPointInteraction.js
+++ views/js/pciCreator/dev/graphLineAndPointInteraction/runtime/graphLineAndPointInteraction.js
@@ -6,7 +6,7 @@ define(['IMSGlobal/jquery_2_1_1', 'qtiCustomInteractionContext'], function($, qt
             return 'graphLineAndPointInteraction';
         },
         /**
-         * Render the PCI : 
+         * Render the PCI :
          * @param {String} id
          * @param {Node} dom
          * @param {Object} config - json
@@ -18,22 +18,22 @@ define(['IMSGlobal/jquery_2_1_1', 'qtiCustomInteractionContext'], function($, qt
             this.config = config || {};
 
             var $container = $(dom);
-            
+
         },
         /**
          * Programmatically set the response following the json schema described in
          * http://www.imsglobal.org/assessment/pciv1p0cf/imsPCIv1p0cf.html#_Toc353965343
-         * 
+         *
          * @param {Object} interaction
          * @param {Object} response
          */
         setResponse : function(response){
-            
+
         },
         /**
          * Get the response in the json format described in
          * http://www.imsglobal.org/assessment/pciv1p0cf/imsPCIv1p0cf.html#_Toc353965343
-         * 
+         *
          * @param {Object} interaction
          * @returns {Object}
          */
@@ -46,7 +46,7 @@ define(['IMSGlobal/jquery_2_1_1', 'qtiCustomInteractionContext'], function($, qt
         /**
          * Remove the current response set in the interaction
          * The state may not be restored at this point.
-         * 
+         *
          * @param {Object} interaction
          */
         resetResponse : function(){
@@ -54,9 +54,9 @@ define(['IMSGlobal/jquery_2_1_1', 'qtiCustomInteractionContext'], function($, qt
         },
         /**
          * Reverse operation performed by render()
-         * After this function is executed, only the inital naked markup remains 
+         * After this function is executed, only the inital naked markup remains
          * Event listeners are removed and the state and the response are reset
-         * 
+         *
          * @param {Object} interaction
          */
         destroy : function(){
@@ -66,7 +66,7 @@ define(['IMSGlobal/jquery_2_1_1', 'qtiCustomInteractionContext'], function($, qt
         },
         /**
          * Restore the state of the interaction from the serializedState.
-         * 
+         *
          * @param {Object} interaction
          * @param {Object} serializedState - json format
          */
@@ -76,7 +76,7 @@ define(['IMSGlobal/jquery_2_1_1', 'qtiCustomInteractionContext'], function($, qt
         /**
          * Get the current state of the interaction as a string.
          * It enables saving the state for later usage.
-         * 
+         *
          * @param {Object} interaction
          * @returns {Object} json format
          */
