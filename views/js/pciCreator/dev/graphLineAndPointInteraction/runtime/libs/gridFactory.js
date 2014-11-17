9cfb74b53096116ff02e8cf50dcbed2d705d031d
Thibault Milan <thibault.milan@vesperiagroup.com>
2014-11-17 16:04:42 +0100

    Refactor grid Factory to put some stuff into private declaration

--- views/js/pciCreator/dev/graphLineAndPointInteraction/runtime/libs/gridFactory.js
+++ views/js/pciCreator/dev/graphLineAndPointInteraction/runtime/libs/gridFactory.js
@@ -5,6 +5,7 @@ define([], function(){
      * Grid Factory
      * @param  {Object}    paper               Raphael paper / canvas object
      * @param  {Object}    options             Grid options
+     * @param  {Object}    options.color       color used for the grid's lines
      * @param  {Number}    options.unit        in px, it's the base unit used for your grid
      * @param  {Number}    options.spacingX    On how each units in the x axis you want to repeat your grid
      * @param  {Number}    options.spacingY    On how each units in the y axis you want to repeat your grid
@@ -14,28 +15,54 @@ define([], function(){
      */
     function gridFactory(paper,options){
         if (options.unit === 0){ throw 'unit must be > 0';}
+
+        /** @type {String} Color of the grid's lines */
+        var _color = options.color || '#000',
+        /** @type {Number} Every how many unit you put a new line in X axis */
+        _spacingX = options.spacingX || 1,
+        /** @type {Number} Every how many unit you put a new line in Y axis */
+        _spacingY = options.spacingY || 1,
+        /** @type {Number} Unit in px */
+        _unit = options.unit || 20,
+        /** @type {Number} width in px of the grid */
+        _width = _spacingX * _unit,
+        /** @type {Number} height in px of the grid */
+        _height = _spacingY * _unit;
+
         var obj = {
-            color : options.color || '#000',
-            spacingX : options.spacingX || 1,
-            spacingY : options.spacingY || 1,
-            unit : options.unit || 20,
             children : paper.set(),
             snapping : options.snapping || false,
             /**
-             * return the width of the grid in px
-             * @private
-             * @return {Number} width X in px
+             * Set _spacingX value
+             * @param  {Number} val
              */
-            _width : function(){
-                return this.spacingX * this.unit;
+            setSpacingX : function(val){
+                _spacingX = parseInt(val);
+                _width =  _spacingX * _unit;
             },
             /**
-             * return the height of the grid in px
-             * @private
-             * @return {Number} height Y in px
+             * Set _spacingY value
+             * @param  {Number} val
              */
-            _height : function(){
-                return this.spacingY * this.unit;
+            setSpacingY : function(val){
+                _spacingY = parseInt(val);
+                _height = _spacingY * _unit;
+            },
+            /**
+             * Set _unit value
+             * @param  {Number} val
+             */
+            setUnit : function(val){
+                _unit = parseInt(val);
+                _width =  _spacingX * _unit;
+                _height = _spacingY * _unit;
+            },
+            /**
+             * Set _color value
+             * @param {String} color
+             */
+            setColor : function(color){
+                _color = String(color);
             },
             /**
              * Rendering function
@@ -43,9 +70,9 @@ define([], function(){
             render : function(){
                 var height = window.screen.height,
                 width  = window.screen.width;
-                for(var y = 0; y <= height; y += this._height){
-                    for(var x = 0; x <= width; x += this._width) {
-                        this.children.push(paper.rect(x,y,this._width,this._height).attr('stroke', this.color));
+                for(var y = 0; y <= height; y += _height){
+                    for(var x = 0; x <= width; x += _width) {
+                        this.children.push(paper.rect(x,y,_width,_height).attr('stroke', _color));
                     }
                 }
             },
@@ -56,7 +83,7 @@ define([], function(){
             snap : function(){
                 var self = this;
                 return function(val){
-                    if (self.snapping) {return paper.snapTo(this.unit,val,this.unit);}
+                    if (self.snapping) {return paper.snapTo(_unit,val,_unit);}
                     else {return val;}
                 };
             },
@@ -66,7 +93,7 @@ define([], function(){
              */
             clickable : function(){
                 /** @type {Object} Rectangle Object to cover the all grid area */
-                var clickableArea = paper.rec(0,0, this._width, this._height);
+                var clickableArea = paper.rect(0,0, _width, _height);
                 clickableArea.attr('fill','rgba(0,0,0,0)');
                 this.children.push(clickableArea);
             }
