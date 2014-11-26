d6e8f53996379816a39f435359cafdcec9925af5
ssipasseuth <sam@taotesting.com>
2014-11-26 17:37:42 +0100

    implemented property form for graphs selection

--- views/js/pciCreator/dev/graphFunctionInteraction/creator/widget/states/Question.js
+++ views/js/pciCreator/dev/graphFunctionInteraction/creator/widget/states/Question.js
@@ -4,15 +4,16 @@ define([
     'taoQtiItem/qtiCreator/widgets/helpers/formElement',
     'taoQtiItem/qtiCreator/editor/simpleContentEditableElement',
     'tpl!graphFunctionInteraction/creator/tpl/propertiesForm',
-    'lodash'
-], function(stateFactory, Question, formElement, editor, formTpl, _){
+    'lodash',
+    'jquery'
+], function(stateFactory, Question, formElement, editor, formTpl, _, $){
 
     var StateQuestion = stateFactory.extend(Question, function(){
-        
+
         //code to execute when entering this state
 
     }, function(){
-        
+
         //code to execute when leaving this state
 
     });
@@ -20,38 +21,53 @@ define([
     StateQuestion.prototype.initForm = function(){
 
         //code to init your interaction property form (on the right side bar)
-        
-        var widget = this.widget, 
+
+        var widget = this.widget,
             interaction = widget.element,
             $form = widget.$form,
             response = interaction.getResponseDeclaration(),
-            somePropValue = 'some prop value';
-        
+            graphs = {
+                linear : {label : 'Linear'},
+                absolute : {label : 'Absolute Value'},
+                quadratic : {label : 'Quadratic'},
+                exponential : {label : 'Exponential'},
+                logarithmic : {label : 'Logarithmic'},
+                cosine : {label : 'Sin/Cos'},
+                tangent : {label : 'Tan/Cotan'},
+            };
+
+        var graphSet = interaction.prop('graphs');
+        graphSet = graphSet ? graphSet.split(',') : [];
+        _.each(graphSet, function(graph){
+            graphs[graph].checked = true;
+        });
+
         //render the form using the form template
         $form.html(formTpl({
             serial : response.serial,
-            someProp : somePropValue,
-            identifier : interaction.attr('responseIdentifier')
+            identifier : interaction.attr('responseIdentifier'),
+            graphs : graphs
         }));
-        
+
         //init form javascript
         formElement.initWidget($form);
 
         //init data change callbacks
         formElement.setChangeCallbacks($form, interaction, {
-            someProp : function(interaction, value){
-
-                //update the pci property value:
-                interaction.prop('someProp', value);
-
-                //update rendering (if required to update the visual)
-                //warning heavy operation : might be a good idea to use lodash.throttle()
-                widget.refresh();
-            },
             identifier : function(i, value){
                 response.id(value);
                 interaction.attr('responseIdentifier', value);
             }
+        });
+        
+        var $graphs= $form.find('[name=graphs]');
+        $graphs.on('change', function(){
+            var checked = [];
+            $graphs.filter(':checked').each(function(){
+                checked.push($(this).val());
+            });
+            interaction.prop('graphs', checked.join(','));
+            interaction.triggerPci('functionschange', [checked]);
         });
     };
 
