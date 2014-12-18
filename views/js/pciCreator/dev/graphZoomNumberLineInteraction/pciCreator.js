5b2ff8fb50f4f16e4901c205c91d952b22583c70
Thibault Milan <thibault.milan@vesperiagroup.com>
2014-12-18 17:52:13 +0100

    Default configuration upgrade

--- views/js/pciCreator/dev/graphZoomNumberLineInteraction/pciCreator.js
+++ views/js/pciCreator/dev/graphZoomNumberLineInteraction/pciCreator.js
@@ -9,7 +9,7 @@ define([
     var interactionCreator = {
         /**
          * (required) Get the typeIdentifier of the custom interaction
-         * 
+         *
          * @returns {String}
          */
         getTypeIdentifier : function(){
@@ -18,7 +18,7 @@ define([
         /**
          * (required) Get the widget prototype
          * Used in the renderer
-         * 
+         *
          * @returns {Object} Widget
          */
         getWidget : function(){
@@ -27,7 +27,7 @@ define([
         /**
          * (optional) Get the default properties values of the pci.
          * Used on new pci instance creation
-         * 
+         *
          * @returns {Object}
          */
         getDefaultProperties : function(pci){
@@ -35,21 +35,23 @@ define([
                 intervals : 'closed-closed,closed-open,open-closed,open-open,arrow-open,arrow-closed,open-arrow,closed-arrow',
                 color : '#266d9c',
                 min : -5,
-                max : 5
+                max : 5,unitSubDivision : 2,
+                increment : 1,
+                snapTo : 1
             };
         },
         /**
-         * (optional) Callback to execute on the 
+         * (optional) Callback to execute on the
          * Used on new pci instance creation
-         * 
+         *
          * @returns {Object}
          */
         afterCreate : function(pci){
             //do some stuff
         },
         /**
-         * (required) Gives the qti pci xml template 
-         * 
+         * (required) Gives the qti pci xml template
+         *
          * @returns {function} handlebar template
          */
         getMarkupTemplate : function(){
@@ -57,11 +59,11 @@ define([
         },
         /**
          * (optional) Allows passing additional data to xml template
-         * 
+         *
          * @returns {function} handlebar template
          */
         getMarkupData : function(pci, defaultData){
-            
+
             return _.defaults(defaultData, {
                 prompt : pci.data('prompt')
             });
