538b2d40726ca7e7f57e31ccd1b64116207e1b51
ssipasseuth <sam@taotesting.com>
2015-01-13 11:53:30 +0100

    fixed getCartesianCoord for y-axis

--- install/local/portableSharedLibraries/PARCC/pointFactory.js
+++ install/local/portableSharedLibraries/PARCC/pointFactory.js
@@ -109,7 +109,7 @@ define(['IMSGlobal/jquery_2_1_1', 'OAT/lodash'], function($, _){
                 decimals = (decimals === undefined) ? 0 : decimals;
                 return {
                     x: (_x - grid.getOriginPosition().left)/grid.getUnitSizes().x,
-                    y: (_y - grid.getOriginPosition().top)/grid.getUnitSizes().y
+                    y: -(_y - grid.getOriginPosition().top)/grid.getUnitSizes().y
                 };
             },
             /**
