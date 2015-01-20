2a8504f1c08d14b44b073f90c71a7aea2e7fb840
ssipasseuth <sam@taotesting.com>
2015-01-20 17:18:52 +0100

    integrating solutionSet wrapper

--- views/js/pciCreator/dev/graphLineAndPointInteraction/runtime/graphLineAndPointInteraction.js
+++ views/js/pciCreator/dev/graphLineAndPointInteraction/runtime/graphLineAndPointInteraction.js
@@ -9,7 +9,8 @@ define([
     'graphLineAndPointInteraction/runtime/wrappers/setOfPoints',
     'graphLineAndPointInteraction/runtime/wrappers/points',
     'graphLineAndPointInteraction/runtime/wrappers/lines',
-    'graphLineAndPointInteraction/runtime/wrappers/segments'
+    'graphLineAndPointInteraction/runtime/wrappers/segments',
+    'graphLineAndPointInteraction/runtime/wrappers/solutionSet'
 ], function(
     $,
     qtiCustomInteractionContext,
@@ -21,7 +22,8 @@ define([
     setPointsWrapper,
     pointsWrapper,
     linesWrapper,
-    segmentsWrapper
+    segmentsWrapper,
+    solutionSetWrapper
     ){
 
     'use strict';
@@ -67,25 +69,22 @@ define([
 
         return paper;
     }
-
+    
+    var _wrappers = {
+        setPoints : setPointsWrapper,
+        points : pointsWrapper,
+        lines : linesWrapper,
+        segments : segmentsWrapper,
+        solutionSet : solutionSetWrapper
+    };
+    
     /**
      * Dirty functiion to return the right wrapper for a given config element
      * @param  {String} type     Name of the element you want
      * @return {Object}         Wrapper corresponding to this element
      */
     function getWrapper(type){
-        switch(type){
-            case 'setPoints' :
-                return setPointsWrapper;
-            case 'points' :
-                return pointsWrapper;
-            case 'lines' :
-                return linesWrapper;
-            case 'segments' :
-                return segmentsWrapper;
-            default :
-                throw 'invalid wrapper type';
-        }
+        return _wrappers[type];
     }
 
     function drawLineStyle(dom, config){
@@ -153,7 +152,7 @@ define([
                 });
 
                 $canvas.on('drawn.lines', function(e, line){
-                    drawSolutionSet();
+                    console.log('need to update solution set');
                 });
 
                 return grid;
@@ -231,13 +230,11 @@ define([
                         $controlArea.append($buttonContainer);
 
                         //init element
-                        if(typeName !== 'solutionSet'){
-                            var wrapper = getWrapper(typeName);
-                            var element = wrapper.initialize(grid, elementConfig);
-                            $buttonContainer.data('element', element);
-                            element.$buttonContainer = $buttonContainer;
-                            elements[elementConfig.uid] = element;
-                        }
+                        var wrapper = getWrapper(typeName);
+                        var element = wrapper.initialize(grid, elementConfig);
+                        $buttonContainer.data('element', element);
+                        element.$buttonContainer = $buttonContainer;
+                        elements[elementConfig.uid] = element;
                     });
                 });
 
@@ -279,7 +276,7 @@ define([
                 element.$buttonContainer.siblings('.button-container').removeClass('activated');
 
                 //activate the element itself to allow interaction
-                element.activate();
+                element.activate(elements);
 
                 graph = element;
             }
@@ -336,306 +333,6 @@ define([
                 }
             }
 
-            function getHorizontalEquation(y){
-                y = -y;
-                var equation = [0, y];
-                equation.type = 'linear';
-                return {
-                    equation : equation,
-                    uid : 'horizontal_' + y
-                };
-            }
-
-            function getVerticalEquation(x){
-                var equation = [x];
-                equation.type = 'vertical';
-                return {
-                    equation : equation,
-                    uid : 'vertical_' + x
-                };
-            }
-
-            function _round(num){
-                return Math.round(num * 100) / 100;
-            }
-
-            function intersect(line1, line2){
-
-                var x, y, eq1 = line1.equation, eq2 = line2.equation;
-
-                //parallel lines :
-                if(eq1.type === eq2.type && (eq1.type === 'vertical' || eq1[0] === eq2[0])){
-                    return;
-                }
-
-
-                if(eq1.type === eq2.type){
-                    //linear equation
-                    x = (eq1[1] - eq2[1]) / (eq2[0] - eq1[0]);
-                    y = eq1[0] * x + eq1[1];
-                }else{
-                    //one linear, one vertical
-                    var vertical, linear;
-                    if(eq1.type === 'vertical'){
-                        vertical = eq1;
-                        linear = eq2;
-                    }else{
-                        vertical = eq2;
-                        linear = eq1;
-                    }
-                    x = vertical[0];
-                    y = linear[0] * vertical[0] + linear[1];
-                }
-
-                var intersection = {
-                    uid : _.uniqueId('intersection_'),
-                    x : _round(x),
-                    y : _round(y),
-                    lines : [line1, line2],
-                    initCircling : function initCircling(){
-
-                        var nextDirection, siblings, next;
-
-                        //take the equation that has the highest angle
-                        if(line1.type === 'vertical'){
-                            nextDirection = line2;
-                        }else if(line2.type === 'vertical'){
-                            nextDirection = line1;
-                        }else if(line1.equation[0] > line2.equation[0]){
-                            nextDirection = line1;
-                        }else if(line2.equation[0] > line1.equation[0]){
-                            nextDirection = line2;
-                        }else{
-                            throw 'unexpected situation';
-                        }
-
-                        var bounds = grid.getGridBounds();
-                        if(y > -bounds.y.end){
-                            //allow init for points that are inside the grid
-                            siblings = nextDirection.getSiblings(intersection);
-                            next = siblings.next;
-                        }
-
-                        return {
-                            point : next,
-                            direction : nextDirection,
-                            ascendingX : true
-                        };
-                    },
-                    next : function next(previousDirection, ascendingX){
-
-                        ascendingX = !!ascendingX;
-
-                        var nextDirection,
-                            next;
-
-                        if(previousDirection){
-                            //try changing direction
-                            if(line1.uid === previousDirection.uid){
-                                nextDirection = line2;
-                            }else{
-                                nextDirection = line1;
-                            }
-                        }
-
-                        var siblings = nextDirection.getSiblings(intersection);
-                        if(nextDirection.type === 'linear'){
-                            if(nextDirection.equation[0] > previousDirection.equation[0]){
-                                ascendingX = !ascendingX;
-                            }
-                            next = ascendingX ? siblings.next : siblings.previous;
-                            console.log('debug', ascendingX, previousDirection, siblings);
-                        }else if(nextDirection.type === 'vertical'){
-                            ascendingX = !ascendingX;
-                            next = ascendingX ? siblings.next : siblings.previous;
-                        }
-
-
-                        return {
-                            point : next,
-                            direction : nextDirection,
-                            ascendingX : ascendingX
-                        };
-                    }
-                };
-
-                return intersection;
-
-            }
-
-            function withinGrid(intersection, grid){
-                var bounds = grid.getGridBounds();
-                if(bounds.x.start <= intersection.x && intersection.x <= bounds.x.end
-                    && -bounds.y.end <= intersection.y && intersection.y <= -bounds.y.start){
-                    return true;
-                }
-                return false;
-            }
-
-            function getClosedPath(intersection){
-
-                //determine next point
-                var path = [];
-                var current = intersection.initCircling();
-
-                var i = 0;
-
-                if(current.point){
-                    path.push([intersection.x, intersection.y]);
-                    while(current.point.x !== intersection.x || current.point.y !== intersection.y){
-
-                        //append path
-                        path.push([current.point.x, current.point.y]);
-
-                        //increment
-                        var next = current.point.next(current.direction, current.ascendingX);
-                        var current = next;
-                        i++;
-
-                        //security:
-                        if(i > 20){
-                            break;
-                        }
-                    }
-                }
-
-                return path;
-            }
-
-            function createIntersectingLine(drawnLine){
-
-                var intersections = [];
-                var sorted = false;
-
-                var intersectingLine = {
-                    equation : drawnLine.equation,
-                    type : drawnLine.equation.type,
-                    uid : drawnLine.uid
-                };
-
-                intersectingLine.addIntersection = function addIntersection(intersection){
-                    intersections.push(intersection);
-                    sorted = false;
-                };
-
-                intersectingLine.getSiblings = function getSiblings(intersection){
-
-                    var previous, next, index;
-
-                    if(!sorted){
-                        //sort by ascending "x"
-                        if(drawnLine.equation.type === 'vertical'){
-                            intersections = _.sortBy(intersections, 'y');
-                        }else{
-                            //linear
-                            intersections = _.sortBy(intersections, 'x');
-                        }
-                    }
-
-                    //find the index of the intersection point in the line
-                    index = _.findIndex(intersections, function(i){
-                        return i.uid === intersection.uid;
-                    });
-
-                    //find the previous
-                    if(index === 0){
-                        next = intersections[1];
-                    }else if(index === intersections.length - 1){
-                        previous = intersections[intersections.length - 2];
-                    }else if(index > 0){
-                        previous = intersections[index - 1];
-                        next = intersections[index + 1];
-                    }
-
-                    return {
-                        previous : previous,
-                        next : next
-                    };
-                }
-
-                return intersectingLine;
-            }
-            
-            function zipPath(pathArray){
-                var pathStr = '';
-                _.forIn(pathArray, function(coords, i){
-                    if(i == 0){
-                        pathStr += 'M'
-                    }else{
-                        pathStr += 'L'
-                    }
-                    pathStr += coords[0]+','+coords[1];
-                });
-                pathStr += 'z';
-                return pathStr;
-            }
-            
-            function convertPath(pathArray){
-                
-                var positionArray = []
-                _.each(pathArray, function(coord){
-                    var pos = grid.getPostionFromCartesian(coord[0], coord[1]);
-                    positionArray.push([pos.left, pos.top]);
-                });
-                return positionArray;
-            }
-            
-            function drawSolutionSet(){
-
-                var intersectingLines = [],
-                    intersections = [],
-                    closedPaths = [];
-
-                //get drawn lines and their intersectingLines
-                _.each(_.where(elements, {type : 'line'}), function(line){
-                    var drawnLine = line.getLine();
-                    if(drawnLine){
-                        intersectingLines.push(createIntersectingLine(drawnLine));
-                    }
-                });
-
-                //include graph borders intersectingLines
-                intersectingLines.push(createIntersectingLine(getHorizontalEquation(self.config.y.start)));
-                intersectingLines.push(createIntersectingLine(getHorizontalEquation(self.config.y.end)));
-                intersectingLines.push(createIntersectingLine(getVerticalEquation(self.config.x.start)));
-                intersectingLines.push(createIntersectingLine(getVerticalEquation(self.config.x.end)));
-
-                //get all intersection points (on graph)
-                while(intersectingLines.length){
-                    var line1 = intersectingLines.pop();
-                    _.each(intersectingLines, function(line2){
-                        var intersection = intersect(line1, line2);
-                        if(intersection && withinGrid(intersection, grid)){
-
-                            //add the intersection to the associated lines:
-                            line1.addIntersection(intersection);
-                            line2.addIntersection(intersection);
-
-                            //append interactions stack
-                            intersections.push(intersection);
-                        }
-                    });
-                }
-                console.log(intersections);
-
-                //calculate all shape coord
-                _.each(intersections, function(intersection){
-                    var closedPath = getClosedPath(intersection);
-                    if(closedPath.length){
-                        console.log(closedPath);
-                        closedPath = convertPath(closedPath);
-                        closedPath = zipPath(closedPath);
-                        grid.getCanvas().path(closedPath).attr({
-                            stroke : '#333',
-                            fill : '#ddd',
-                            'fill-opacity' : .6
-                        });
-                    }
-                });
-
-                //draw shape coord
-            }
-
             grid = initGrid($container, this.config);
             initInteraction(grid, $container, this.config);
 
@@ -654,8 +351,6 @@ define([
                 grid = initGrid($container, self.config);
                 initInteraction(grid, $container, self.config);
             });
-
-            drawSolutionSet();
         },
         /**
          * Programmatically set the response following the json schema described in
