f71f122d14dc97c3df249db16c072749e519300b
ssipasseuth <sam@taotesting.com>
2015-02-19 15:16:37 +0100

    separate tabbing, scrolling and paging behaviour in their own functions

--- views/js/pciCreator/dev/multiTabbedExhibit/runtime/multiTabbedExhibit.js
+++ views/js/pciCreator/dev/multiTabbedExhibit/runtime/multiTabbedExhibit.js
@@ -16,7 +16,128 @@ define([
     function buildConfig(config) {
         return config;
     }
+    
+    function initPaging($frameContainer){
+        
+        var $counterCurrent = $frameContainer.find('.counter-current');
+            var $counterTotal = $frameContainer.find('.counter-total');
+            var $pages = $frameContainer.find('.page');
+            var pages = [];
 
+            $pages.each(function(){
+                var $page = $(this);
+                var pos = $page.position();
+                var h = $page.outerHeight();
+                pages.push({
+                   top :  Math.round(pos.top),
+                   middle : Math.round(pos.top + h/2),
+                   bottom : Math.round(pos.top + h)
+                });
+            });
+            $counterTotal.html(pages.length);
+
+            //add page manager:
+            var moveEnd = _.throttle(function(){
+
+                var pos = this.pos;
+                var currentPage = 1;
+
+                //check position of the pages
+                for(var i in pages){
+                    i = parseInt(i);
+                    if(pos.cur > pages[i].middle && (!pages[i+1] || pos.cur < pages[i+1].middle)){
+                        currentPage = i+2;
+                        break;
+                    }
+                }
+
+                //set current page:
+                if(currentPage){
+                    $counterCurrent.html(currentPage);
+                }
+            },400);
+            
+            return {
+                getMoveCallback : function getMoveEndCallback(){
+                    return moveEnd;
+                }
+            };
+    }
+    
+    function initScrolling($frameContainer){
+        
+        if(!$frameContainer.hasClass('frame-container')){
+            throw 'the container has not the right css class';
+        }
+        
+        var $frame = $frameContainer.children('.frame');
+        var $scrollbar = $frameContainer.children('.scrollbar');
+        var events = {};
+
+        if($frameContainer.hasClass('passage-paging')){
+            var pagging = initPaging($frameContainer);
+            events.moveEnd = pagging.getMoveCallback();
+        }
+        
+        //init the sly scrollbar
+        $frame.sly({
+            scrollBar: $scrollbar,
+            scrollBy: 20,
+            scrollTrap: true,
+            dynamicHandle : true,
+            clickBar : true,
+            dragHandle: true,
+            mouseDragging: false
+        },events);
+                
+    }
+    
+    function initTabbing($tabContainer){
+        
+        if(!$tabContainer.hasClass('passages-tabs')){
+            throw 'the container has not the right css class';
+        }
+        
+        var $tabs = $tabContainer.children('.passage');
+        var $nav = $tabContainer.children('.passages-tab-navigation');
+        var i = 0;
+        var $active = $nav.find('li:first-child a');
+
+        $tabs.hide();
+        $nav.children('li').each(function () {
+            var $li = $(this),
+                    $a = $li.children('a');
+
+            if (i < $tabs.length) {
+                var $tab = $($tabs[i]);
+                $a.data('passage-tab', $tab);
+
+                if ($li.hasClass('passages-tab-active')) {
+                    $active = $a;
+                }
+            } else {
+                $a.hide();
+            }
+            i++;
+        });
+        $nav.on('click', 'a', function () {
+            activateTab($(this));
+        });
+        //activate initial tab:
+        activateTab($active);
+
+        function activateTab($a){
+            var $li = $a.parent();
+            var $tab = $a.data('passage-tab');
+            if ($tab && $tab.length) {
+                //toggle visibility:
+                $tab.show().siblings('.passage').hide();
+                //change li class:
+                $li.addClass('passages-tab-active').siblings('li').removeClass('passages-tab-active');
+            }
+        }
+    }
+    
     var multiTabbedExhibit = {
         id: -1,
         getTypeIdentifier: function () {
@@ -37,107 +158,15 @@ define([
             //add method on(), off() and trigger() to the current object
             event.addEventMgr(this);
 
-            //init scrolling
+            //init scrolling on all "scrollable" frame container
             $('.frame-container').each(function () {
-
-                var $frameContainer = $(this);
-                var $frame = $frameContainer.children('.frame');
-                var $scrollbar = $frameContainer.children('.scrollbar');
-                var events = {};
-                
-                if($frameContainer.hasClass('passage-paging')){
-                    
-                    var $counterCurrent = $frameContainer.find('.counter-current');
-                    var $counterTotal = $frameContainer.find('.counter-total');
-                    var $pages = $frameContainer.find('.page');
-                    var pages = [];
-                    
-                    $pages.each(function(){
-                        var $page = $(this);
-                        var pos = $page.position();
-                        var h = $page.outerHeight();
-                        pages.push({
-                           top :  Math.round(pos.top),
-                           middle : Math.round(pos.top + h/2),
-                           bottom : Math.round(pos.top + h)
-                        });
-                    });
-                    $counterTotal.html(pages.length);
-                    
-                    //add page manager:
-                    events.moveEnd = _.throttle(function(){
-                        
-                        var pos = this.pos;
-                        var currentPage = 1;
-                        
-                        //check position of the pages
-                        for(var i in pages){
-                            i = parseInt(i);
-                            if(pos.cur > pages[i].middle && (!pages[i+1] || pos.cur < pages[i+1].middle)){
-                                currentPage = i+2;
-                                break;
-                            }
-                        }
-                        
-                        //set current page:
-                        if(currentPage){
-                            $counterCurrent.html(currentPage);
-                        }
-                    },400);
-                }
-                
-                //init the sly scrollbar
-                $frame.sly({
-                    scrollBar: $scrollbar,
-                    scrollBy: 20,
-                    scrollTrap: true,
-                    dynamicHandle : true,
-                    clickBar : true,
-                    dragHandle: true,
-                    mouseDragging: false
-                },events);
+                initScrolling($(this));
             });
 
             //init tabbing
             var $tabContainer = $('.passages-tabs');
-            var $tabs = $tabContainer.children('.passage');
-            var $nav = $tabContainer.children('.passages-tab-navigation');
-            var i = 0;
-            var $active = $nav.find('li:first-child a');
+            initTabbing($tabContainer);
             
-            $tabs.hide();
-            $nav.children('li').each(function () {
-                var $li = $(this),
-                        $a = $li.children('a');
-
-                if (i < $tabs.length) {
-                    var $tab = $($tabs[i]);
-                    $a.data('passage-tab', $tab);
-
-                    if ($li.hasClass('passages-tab-active')) {
-                        $active = $a;
-                    }
-                } else {
-                    $a.hide();
-                }
-                i++;
-            });
-            $nav.on('click', 'a', function () {
-                activateTab($(this));
-            });
-            //activate initial tab:
-            activateTab($active);
-            
-            function activateTab($a){
-                var $li = $a.parent();
-                var $tab = $a.data('passage-tab');
-                if ($tab && $tab.length) {
-                    //toggle visibility:
-                    $tab.show().siblings('.passage').hide();
-                    //change li class:
-                    $li.addClass('passages-tab-active').siblings('li').removeClass('passages-tab-active');
-                }
-            }
         },
         /**
          * Programmatically set the response following the json schema described in
