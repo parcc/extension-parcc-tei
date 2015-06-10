5f01ede2d6ed498ecb4719bc751db71aeba2d633
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2015-06-10 15:41:58 +0200

    add grunt task to package parcc TEIs

---
+++ views/build/grunt/bundle.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+
+    var requirejs   = grunt.config('requirejs') || {};
+
+    var root        = grunt.option('root');
+    var out         = 'output';
+
+    var teis = [
+        'fractionModelInteraction/runtime/fractionModelInteraction',
+        'graphFunctionInteraction/runtime/graphFunctionInteraction',
+        'graphLineAndPointInteraction/runtime/graphLineAndPointInteraction',
+        'graphNumberLineInteraction/runtime/graphNumberLineInteraction',
+        'graphZoomNumberLineInteraction/runtime/graphZoomNumberLineInteraction',
+        'histogramInteraction/runtime/histogramInteraction',
+        'interactivePassageInteraction/runtime/interactivePassageInteraction',
+        'multiTabbedExhibit/runtime/multiTabbedExhibit',
+    ];
+
+    /**
+     * Compile the new item runner as a standalone library
+     */
+    requirejs.tei = {
+        options: {
+            baseUrl : root + '/parccTei/views/js/pciCreator/dev/',
+            findNestedDependencies : true,
+            //optimize : 'none',
+            uglify2: {
+                mangle : false,
+                output: {
+                    'max_line_len': 400
+                }
+            },
+            paths : {
+                'IMSGlobal' : root + '/taoQtiItem/views/js/portableSharedLibraries/IMSGlobal',
+                'OAT' : root + '/taoQtiItem/views/js/portableSharedLibraries/OAT',
+                'PARCC' : root + '/taoQtiItem/views/js/portableSharedLibraries/PARCC',
+                'qtiCustomInteractionContext' : root + '/taoQtiItem/views/js/runtime/qtiCustomInteractionContext'
+            },
+            wrapShim: true,
+            inlineCss : true,
+            include : teis,
+            exclude : ['qtiCustomInteractionContext'],
+            out: out + "/tei.min.js"
+        }
+    };
+
+
+
+    grunt.config('requirejs', requirejs);
+
+
+};
