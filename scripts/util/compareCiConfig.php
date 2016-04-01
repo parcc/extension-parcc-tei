df2cb3e2a514e07746c44fde5272cb4bd11f6d03
ssipasseuth <sam@taotesting.com>
2016-04-01 10:23:23 +0200

    added script to compare configs

---
+++ scripts/util/compareCiConfig.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2016 (original work) Open Assessment Technologies SA ;
+ *
+ */
+$ci_configs = [];
+$ci_configs['VF558770'] = '{"div":"GRAPH","width":500,"height":500,"graphType":"scatterPlot","gridType":"oneQuadrant","vLines":8,"hLines":8,"segmentMode":false,"outerLineWeight":3,"innerLineWeight":1,"plotLineWeight":6,"plotLineColor":"#0000FF","pointRadius":8,"pointHalo":true,"pointColor":"#0000FF","splitScreen":true,"pointRules":{"maximumPoints":3,"allowOuterH":false,"allowOuterV":false,"xSubIncrement":1,"ySubIncrement":1,"draggable":true},"xAxis":{"startPoint":0,"increment":1},"yAxis":{"startPoint":0,"increment":1}}';
+$ci_configs['VF643134'] = '{"div":"graph","width":500,"height":500,"graphType":"line","gridType":"oneQuadrant","vLines":11,"hLines":13,"segmentMode":false,"outerLineWeight":3,"innerLineWeight":1,"plotLineWeight":6,"plotLineColor":"#0000FF","pointRadius":8,"pointHalo":true,"pointColor":"#0000FF","splitScreen":true,"pointRules":{"maximumPoints":2,"allowOuterH":true,"allowOuterV":true,"xSubIncrement":1,"ySubIncrement":1,"draggable":true},"xAxis":{"startPoint":0,"increment":25,"label":"Height of Drop (in inches)","axisLabel":"x"},"yAxis":{"startPoint":0,"increment":25,"label":"Height of Bounce (in inches)","axisLabel":"y"},"graphTitle":""}';
+$ci_configs['VF643560'] = '{"div":"graph1","width":500,"height":500,"graphType":"line","gridType":"coordinate","vLines":14,"hLines":14,"segmentMode":false,"outerLineWeight":3,"innerLineWeight":1,"plotLineWeight":6,"plotLineColor":"#0000FF","pointRadius":8,"pointHalo":true,"pointColor":"#0000FF","splitScreen":true,"pointRules":{"maximumPoints":2,"allowOuterH":true,"allowOuterV":true,"xSubIncrement":1,"ySubIncrement":1,"draggable":true},"xAxis":{"startPoint":-6,"increment":1,"axisLabel":"x","title":""},"yAxis":{"startPoint":-6,"increment":1,"axisLabel":"y","title":""},"graphTitle":{"type":"string","description":"Title for Graph.","required":false}}';
+$ci_configs['VF647028'] = '{"div":"graph","width":500,"height":500,"graphType":"line","gridType":"oneQuadrant","vLines":10,"hLines":10,"segmentMode":false,"outerLineWeight":3,"innerLineWeight":1,"plotLineWeight":6,"plotLineColor":"#0000FF","pointRadius":8,"pointHalo":true,"pointColor":"#0000FF","splitScreen":true,"pointRules":{"maximumPoints":2,"allowOuterH":true,"allowOuterV":true,"xSubIncrement":1,"ySubIncrement":1,"draggable":true},"xAxis":{"startPoint":0,"increment":1,"title":"Time (seconds)","axisLabel":"t"},"yAxis":{"startPoint":0,"increment":1,"title":"Distance (meters)","axisLabel":"d"},"graphTitle":{"type":"string","description":"Title for Graph.","required":false}}';
+$ci_configs['VF798694'] = '{"div":"graph","width":700,"height":500,"graphType":"scatterPlot","gridType":"coordinate","vLines":22,"hLines":22,"segmentMode":false,"outerLineWeight":3,"innerLineWeight":1,"plotLineWeight":6,"plotLineColor":"#0000FF","pointRadius":8,"pointHalo":true,"pointColor":"#0000FF","splitScreen":true,"pointRules":{"maximumPoints":1,"allowOuterH":true,"allowOuterV":true,"xSubIncrement":1,"ySubIncrement":1,"draggable":true},"xAxis":{"startPoint":-10,"increment":1},"yAxis":{"startPoint":-10,"increment":1},"graphTitle":{"type":"string","description":"Title for Graph.","required":false}}';
+$ci_configs['VF803457'] = '{"div":"graph","width":500,"height":500,"graphType":"scatterPlot","gridType":"coordinate","vLines":22,"hLines":18,"segmentMode":false,"outerLineWeight":3,"innerLineWeight":1,"plotLineWeight":6,"plotLineColor":"#0000FF","pointRadius":8,"pointHalo":true,"pointColor":"#0000FF","splitScreen":true,"pointRules":{"maximumPoints":1,"allowOuterH":true,"allowOuterV":true,"xSubIncrement":1,"ySubIncrement":1,"draggable":true},"xAxis":{"startPoint":-5,"increment":1,"axisLabel":"x"},"yAxis":{"startPoint":-7,"increment":1,"axisLabel":"y"},"graphTitle":{"type":"string","description":"Title for Graph.","required":false}}';
+$ci_configs['VF821999'] = '{"div":"graph1","width":400,"height":400,"graphType":"line","gridType":"oneQuadrant","vLines":15,"hLines":15,"segmentMode":false,"outerLineWeight":3,"innerLineWeight":1,"plotLineWeight":6,"plotLineColor":"#0000FF","pointRadius":8,"pointHalo":true,"pointColor":"#0000FF","splitScreen":true,"pointRules":{"maximumPoints":2,"allowOuterH":true,"allowOuterV":true,"xSubIncrement":2,"ySubIncrement":2,"draggable":true},"xAxis":{"startPoint":0,"increment":1,"axisLabel":"x","title":"Number of Weeks"},"yAxis":{"startPoint":0,"increment":10,"axisLabel":"y","title":"Number of Dollars Saved"}}';
+$ci_configs['VF905610'] = '{"div":"graph","width":700,"height":500,"graphType":"line","gridType":"coordinate","vLines":16,"hLines":16,"segmentMode":false,"outerLineWeight":3,"innerLineWeight":1,"plotLineWeight":6,"plotLineColor":"#0000FF","pointRadius":8,"pointHalo":true,"pointColor":"#0000FF","splitScreen":true,"pointRules":{"maximumPoints":2,"allowOuterH":false,"allowOuterV":false,"xSubIncrement":1,"ySubIncrement":1,"draggable":true},"xAxis":{"startPoint":-7,"increment":1,"axisLabel":"x"},"yAxis":{"startPoint":-7,"increment":1,"axisLabel":"y"},"graphTitle":{"type":"string","description":"Title for Graph.","required":false}}';
+$ci_configs['VH001116'] = '{"div":"RESPONSE","width":300,"height":340,"graphType":"line","gridType":"oneQuadrant","vLines":9,"hLines":9,"segmentMode":false,"outerLineWeight":3,"innerLineWeight":1,"plotLineWeight":6,"plotLineColor":"#0000FF","pointRadius":8,"pointHalo":true,"pointColor":"#0000FF","splitScreen":true,"pointRules":{"maximumPoints":2,"allowOuterH":true,"allowOuterV":true,"xSubIncrement":2,"ySubIncrement":2,"draggable":true},"xAxis":{"startPoint":0,"increment":5,"title":"Number of Days"},"yAxis":{"startPoint":0,"increment":2,"title":"Height (inches)"}}';
+
+$props = [];
+foreach($ci_configs as $c){
+    $conf = json_decode( $c, true);
+    $props = array_merge_recursive($props, $conf);
+}
+
+$ci_styleguide = [];
+$ci_styleguide['scatterplot'] = '{"div":"RESPONSE","width":300,"height":300,"graphType":"scatterPlot","gridType":"oneQuadrant","vLines":10,"hLines":10,"segmentMode":false,"outerLineWeight":3,"innerLineWeight":1,"plotLineWeight":6,"plotLineColor":"#0000FF","pointRadius":8,"pointHalo":true,"pointColor":"#0000FF","splitScreen":true,"arrowHead":true,"pointRules":{"maximumPoints":6,"allowOuterH":false,"allowOuterV":false,"xSubIncrement":2,"ySubIncrement":2,"draggable":true},"xAxis":{"startPoint":0,"increment":10,"title":"Distance to Park (miles)"},"yAxis":{"startPoint":0,"increment":1,"title":"Number of Visits per Year"}}';
+$ci_styleguide['line'] = '{"div":"RESPONSE","width":300,"height":340,"graphType":"line","gridType":"oneQuadrant","vLines":7,"hLines":10,"segmentMode":true,"outerLineWeight":3,"innerLineWeight":1,"plotLineWeight":6,"plotLineColor":"#0000FF","pointRadius":8,"pointHalo":true,"pointColor":"#0000FF","splitScreen":true,"arrowHead":true,"pointRules":{"maximumPoints":6,"allowOuterH":true,"allowOuterV":true,"xSubIncrement":2,"ySubIncrement":2,"draggable":true},"xAxis":{"startPoint":0,"increment":1,"title":"Time (hr)"},"yAxis":{"startPoint":0,"increment":10,"title":"Number of Cells (x1,000,000)"}}';
+$ci_styleguide['arrow'] = '{"div":"RESPONSE","width":300,"height":340,"graphType":"line","gridType":"oneQuadrant","vLines":7,"hLines":10,"segmentMode":false,"outerLineWeight":3,"innerLineWeight":1,"plotLineWeight":6,"plotLineColor":"#0000FF","pointRadius":8,"pointHalo":true,"pointColor":"#0000FF","splitScreen":true,"arrowHead":true,"pointRules":{"maximumPoints":2,"allowOuterH":true,"allowOuterV":true,"xSubIncrement":2,"ySubIncrement":2,"draggable":true},"xAxis":{"startPoint":0,"increment":1,"title":"Time (hr)"},"yAxis":{"startPoint":0,"increment":10,"title":"Distance"}}';
+//$ci_styleguide['polygon'] = '{"div":"RESPONSE","width":600,"height":450,"graphType":"polygon","gridType":"coordinate","closeShape":{"button":"min","buttonHAlignment":"center","buttonVPadding":10},"segmentMode":false,"vLines":32,"hLines":22,"outerLineWeight":3,"innerLineWeight":1,"plotLineWeight":6,"plotLineColor":"#0000FF","pointRadius":8,"pointHalo":true,"pointColor":"#0000FF","arrowHead":true,"pointRules":{"maximumPoints":4,"allowOuterH":true,"allowOuterV":true,"xSubIncrement":2,"ySubIncrement":2,"draggable":true},"staticPoints":{"pointColor":"#AA00AA","lineColor":"#AA00AA","showLine":true,"displayPoints":true,"points":[{"x":0,"y":0,"label":"A"},{"x":2,"y":4,"label":"B"},{"x":5,"y":4,"label":"C"},{"x":8,"y":0,"label":"D"}]},"xAxis":{"startPoint":0,"increment":1},"yAxis":{"startPoint":0,"increment":1}}';
+
+$props2 = [];
+foreach($ci_styleguide as $c){
+    $conf = json_decode( $c, true);
+    $props2 = array_merge_recursive($props2, $conf);
+}
+
+$propsAll = array_merge_recursive($props, $props2);
+
+print_r($props);
+print_r($props2);
+print_r($propsAll);
+var_dump(count($props), count($props2), count($propsAll), array_diff(array_keys($props), array_keys($props2)), array_diff(array_keys($props2), array_keys($props)));
