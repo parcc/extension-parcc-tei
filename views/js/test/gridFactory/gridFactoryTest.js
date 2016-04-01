30ecddf80297fdfaac08aff7e5afe79ac0706b5a
Christophe Noël <christophe@taotesting.com>
2016-04-01 15:54:10 +0200

    convert gridFactory Unit tests to assertions

--- views/js/test/gridFactory/gridFactoryTest.js
+++ views/js/test/gridFactory/gridFactoryTest.js
@@ -4,10 +4,170 @@ define([
     'jquery'
 ], function(gridFactory, scaleRaphael, $){
 
-    console.log(gridFactory);
+    QUnit.module('gridFactory');
 
+    var unitSize = 20;
+    var gridParameters = [
+        { title: 'simple', input: {
+            x : {
+                start : -6,
+                end : 10,
+                unit : unitSize
+            },
+            y : {
+                start : -10,
+                end : 6,
+                unit : unitSize
+            }
+        }, outputContainer: '.test_01' },
+
+        { title: 'x > 0', input: {
+            x : {
+                start : 5,
+                end : 10,
+                unit : unitSize
+            },
+            y : {
+                start : -6,
+                end : 10,
+                unit : unitSize
+            }
+        }, outputContainer: '.test_02' },
+
+        { title: 'x < 0', input: {
+            x : {
+                start : -10,
+                end : -2,
+                unit : unitSize
+            },
+            y : {
+                start : -6,
+                end : 10,
+                unit : unitSize
+            }
+        }, outputContainer: '.test_03' },
+
+        { title: 'y > 0', input: {
+            x : {
+                start : -6,
+                end : 10,
+                unit : unitSize
+            },
+            y : {
+                start : 2,
+                end : 6,
+                unit : unitSize
+            }
+        }, outputContainer: '.test_04' },
+
+        { title: 'y < 0', input: {
+            x : {
+                start : -6,
+                end : 10,
+                unit : unitSize
+            },
+            y : {
+                start : -8,
+                end : -1,
+                unit : unitSize
+            }
+        }, outputContainer: '.test_05' },
+
+        { title: 'x > 0 && y > 0', input: {
+            x : {
+                start : 5,
+                end : 10,
+                unit : unitSize
+            },
+            y : {
+                start : 5,
+                end : 10,
+                unit : unitSize
+            }
+        }, outputContainer: '.test_06' },
+
+        { title: 'x > 0 && y < 0', input: {
+            x : {
+                start : 5,
+                end : 10,
+                unit : unitSize
+            },
+            y : {
+                start : -10,
+                end : -5,
+                unit : unitSize
+            }
+        }, outputContainer: '.test_07' },
+
+        { title: 'x < 0 && y < 0', input: {
+            x : {
+                start : -10,
+                end : -5,
+                unit : unitSize
+            },
+            y : {
+                start : -10,
+                end : -5,
+                unit : unitSize
+            }
+        }, outputContainer: '.test_08' },
+
+        { title: 'x < 0 && y > 0', input: {
+            x : {
+                start : -10,
+                end : -5,
+                unit : unitSize
+            },
+            y : {
+                start : 5,
+                end : 10,
+                unit : unitSize
+            }
+        }, outputContainer: '.test_09' },
+
+        { title: 'different scales', input: {
+            // height: 500,
+            // width: 500,
+            x : {
+                start : 0,
+                end : 10,
+                unit: unitSize,
+                step: 10
+            },
+            y : {
+                start : -100,
+                end : 0,
+                unit: unitSize,
+                step : 10
+            }
+        }, outputContainer: '.test_10' }
+    ];
+
+    QUnit
+        .cases(gridParameters)
+        .test('grid rendering', function test(data, assert) {
+            var $container = $('<div class="test ' + data.outputContainer + '_actual"><h3 class="title">' + data.title + '</h2><pre class="grid-config"></pre><div class="shape-container"></div>');
+
+            $container.find('.grid-config').append(formatJson(data.input));
+
+            var grid = initGrid($container, data.input);
+
+            $('#paper').append($container);
+
+            assert.ok(
+                $container.find('.shape-container').html() ===
+                $(data.outputContainer + ' .shape-container').html());
+        });
+
+    function initGrid($container, gridConfig){
+
+        var paper = createCanvas($container, gridConfig);
+        var grid = gridFactory(paper, gridConfig);
+
+        return grid;
+    }
     /**
-     * Create the minimum canvas and desplay it
+     * Create the minimum canvas and display it
      * @param  {Object} $container jQuery node
      * @param  {Object} config     configuration ( cleaned )
      * @return {Object}            Paper ( RaphaelJS )
@@ -41,7 +201,7 @@ define([
         var $container = $('<div class="test"><h3 class="title">' + testName + '</h2><pre class="grid-config"></pre><div class="shape-container"></div></div>');
         $container.find('.grid-config').append(formatJson(gridConfig));
         var grid = initGrid($container, gridConfig);
-        
+
         $('#paper').append($container);
     }
 
